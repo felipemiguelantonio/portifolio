ce0b4f101de6ec6523ed4dd1be2e808c67939065
felipe <felipecamisa13@hotmail.com>
2019-06-12 12:41:36 -0300

    primeiro projeto

---
+++ calcula-imc.js
@@ -0,0 +1,43 @@
+
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    
+   if(peso <= 0 || peso > 300){
+       tdPeso.textContent="Peso invalido!";
+          tdImc.textContent="?";
+       
+      if(altura <=0 || altura >3.00){
+         tdAltura.textContent="Altura invalida!";
+          tdImc.textContent="?";
+         }
+       
+      }
+     else{
+         var imc = calculaImc(peso,altura);
+         tdImc.textContent = imc;
+  
+       }
+      
+}
+
+function calculaImc(peso, altura) {
+    var imc;
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
+
+
